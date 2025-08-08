6bbce8d4c38a1ec0294982bf8063a3f25ae8fd06
Kenzo douchet <kenzodouchet33@gmail.com>
2025-08-08 13:35:00 +0200

    add db file connexion

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+function dbLog() {
+  $host = "localhost";
+  $dbname = "todoolist";
+  $user = "root";
+  $password = "root";
+  $port = "8889";
+
+  try {
+    $dsn = "mysql:host=$host;dbname=$dbname;port=$port";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+    die("Erreur de connexion à la db :".$e->getMessage());
+  }
+}
